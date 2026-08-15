231855da162e6d4bb86b25fd17ca46997baee0d2
Logan Lessen <loganlessen@gmail.com>
2026-08-15 14:35:11 -0600

    refactor: refactor and style

--- src/content/post-4.md
+++ src/content/post-4.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/BlogPostLayout.astro
 title: My Fourth Blog Post
 author: Astro Learner
 description: "This post will show up on its own!"
@@ -10,3 +9,7 @@ pubDate: 2022-08-08
 tags: ["astro", "successes"]
 ---
 This post should show up with my other blog posts, because `import.meta.glob()` is returning a list of all my posts in order to create my list.
+
+> THIS IS A TEST
+
+**AAAAAAAAAAAAAAAAA**
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,16 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
-  return rss({
-    title: 'LeBlog',
-    description: ':(){ :|:& };:',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
+    const posts = await getCollection("blog");
+    return rss({
+        title: 'LeBlog',
+        description: ':(){ :|:& };:',
+        site: context.site,
+        items: posts.map(post => ({
+            ...post.data,
+            link: `blog/posts/${post.id}`
+        })),
+        customData: `<language>en-us</language>`,
+    });
 }
